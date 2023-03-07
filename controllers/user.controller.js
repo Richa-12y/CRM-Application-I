b5c0235afc7f7025519fcb97d9f73cd16b26f316
Richa-12y <kricha000@gmail.com>
2023-03-07 20:59:06 +0530

    added the folder str

---
+++ controllers/user.controller.js
@@ -0,0 +1,9 @@
+/**
+ * This file will contain the logic to fetch all users
+ *
+ *  1. Is a valid user
+ *  2. He should be admin
+ *
+ * Above validation should be done as the part of middlware
+ *
+ */
