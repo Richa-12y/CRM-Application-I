77194193ee02ca531b1ac0f3d755d31e9b30a934
Richa-12y <kricha000@gmail.com>
2023-03-12 13:14:29 +0530

    working on  middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,9 +11,25 @@
 const User = require("../model/user.model");
 const objectConverter = require("../utils/objectConverter");
 
-exports.findAll = (req, res) => {
+exports.findAll = async (req, res) => {
   try {
-    const users = User.find({});
+    /**
+     * Read the query params if any
+     */
+
+    const queryObj = {};
+    const userTypeQ = req.query.queryObj;
+    if (userTypeQ) {
+      queryObj.userTypeQ = userTypeQ;
+    }
+
+    const userStatusQ = req.query.userStatus;
+    if (userStatusQ) {
+      queryObj.userStatus = userStatusQ;
+    }
+    const users = await User.find(queryObj);
+
+    const user = User.find({});
     res.status(200).send(objectConverter.userResponse(users));
   } catch (error) {
     console.log("Error while fetching the users", error.message);
