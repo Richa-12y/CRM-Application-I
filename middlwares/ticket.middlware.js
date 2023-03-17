c0c844bd98448ae2dff0cf11718157b13828d4b5
Richa-12y <kricha000@gmail.com>
2023-03-17 16:47:06 +0530

    working on middleware tickets

---
+++ middlwares/ticket.middlware.js
@@ -0,0 +1,33 @@
+const validateTicketReqBody = (req, res, next) => {
+  /**
+   * Check if body.req.title is not present it is bad request
+   * write a condition for that
+   */
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Title of the ticket is missing",
+    });
+  }
+  /**
+   * Check if body.req.description is not present it is bad request
+   * write a condition for that
+   */
+  if (!req.body.description) {
+    res.status(400).send({
+      message: "Description of the ticket is missing",
+    });
+  }
+  /**
+   * Check if body.req.status is present
+   * write a condition for that
+   */
+
+  if (req.body.status != undefined) {
+    if (!["OPEN", "IN_PROGRESS", "CLOSED"].includes(req.body.status)) {
+      return res.status(400).send({
+        message: "Not a valid status",
+      });
+    }
+  }
+  next();
+};
